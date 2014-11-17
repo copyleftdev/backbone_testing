ce47ce4a93fff04f3cc544caf8a9c87c85deefe4
Don Johnson <dj@codetestcode.io>
2014-11-17 13:31:21 -0800

    created tests for fails

--- test/chapters/01/js/spec/failure.spec.js
+++ test/chapters/01/js/spec/failure.spec.js
@@ -1,31 +1,11 @@
-<html>
-  <head>
-    <title>Backbone.js Testing Chapter 01</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <!-- Mocha Styles -->
-    <link rel="stylesheet" href="../../js/lib/mocha.css"/>
+mocha.bail(false);
 
-    <!-- Testing Libraries -->
-    <script src="../../js/lib/mocha.js"></script>
-    <script src="../../js/lib/chai.js"></script>
-    <script src="../../js/lib/sinon.js"></script>
+describe("Test failures", function() {
+    it("should fail on assertion", function() {
+        expect("hi").to.equal("goodbye");
+    });
 
-    <!-- Test Setup -->
-    <script>
-    //Setup.
-    var expect = chai.expect;
-    mocha.setup("bdd");
-    //Run tests on windows load event
-    window.onload = function() {
-      (window.mochaPhantomJS || mocha).run();
-    };
-    </script>
-    <!-- !Tests. -->
-    <script src="js/spec/failure.spec.js"></script>
-  </head>
-  <body>
-    <div id="mocha"></div>
-  </body>
-
-</html>
+    it("should fail on unexpected exception", function() {
+        throw new Error();
+    });
+});
